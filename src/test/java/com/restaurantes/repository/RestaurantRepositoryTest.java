a737a03f2ba7369ab3828c82e0517efc47a4fd6a
alansastre <alan@certidevs.com>
2026-04-08 19:49:11 +0200

    RestaurantRepositoryTest

--- src/test/java/com/restaurantes/repository/RestaurantRepositoryTest.java
+++ src/test/java/com/restaurantes/repository/RestaurantRepositoryTest.java
@@ -1,17 +1,32 @@
 package com.restaurantes.repository;
 
+import com.restaurantes.model.Restaurant;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class RestaurantRepositoryTest {
 
-
-    // RestaurantRepository repository;
+    @Autowired
+    RestaurantRepository restaurantRepository;
 
     @Test
     void guardarRestaurante() {
 
-        // repository.save()
+        // INSERT INTO restaurantes
+         Restaurant restaurant = new Restaurant();
+        // RestaurantRepository repo = new RestaurantRepository(); // No se puede porque es una interface no una class
+
+//        Restaurant restaurant = Restaurant.builder().build();
+        restaurant.setName("Restaurante");
+        restaurant.setAveragePrice(20.0);
+        restaurant = restaurantRepository.save(restaurant);
+
+        assertNotNull(restaurant.getId());
+        assertEquals("Restaurante", restaurant.getName());
+
     }
 }
