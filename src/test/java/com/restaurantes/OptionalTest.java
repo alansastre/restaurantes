e45ca56f9f1b2a1fb0d42be6449ad4fac1168829
alansastre <alan@certidevs.com>
2026-04-13 18:33:40 +0200

    optional pruebas no lo leas por favor

--- src/test/java/com/restaurantes/OptionalTest.java
+++ src/test/java/com/restaurantes/OptionalTest.java
@@ -1,5 +1,10 @@
 package com.restaurantes;
 
+import com.restaurantes.model.Employee;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
 public class OptionalTest {
 
     // Mecanismo de Null Safety
@@ -7,6 +12,12 @@ public class OptionalTest {
     // Optional
 
     // of
+    @Test
+    void comoCrearUnOptional() {
+
+        Employee emp1 = new Employee();
+        Optional<Employee> optional = Optional.of(emp1);
+    }
 
     // ofNullable
 
