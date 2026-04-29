2187a4a1bef9c08659183f7be1e2e597387bd577
alansastre <alan@certidevs.com>
2026-04-29 18:31:50 +0200

    #8 datos demo crear data initializer

---
+++ src/main/java/com/restaurantes/config/DataInitializer.java
@@ -0,0 +1,13 @@
+package com.restaurantes.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("HOLA DESDE DATA INITIALIZER");
+    }
+}
