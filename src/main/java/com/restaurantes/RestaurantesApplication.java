2c7d021cb881d561d0e809b9bc5dedfcf4bd2f2d
alansastre <alan@certidevs.com>
2026-04-29 19:38:56 +0200

    #8 data initializer y profile test completado

--- src/main/java/com/restaurantes/RestaurantesApplication.java
+++ src/main/java/com/restaurantes/RestaurantesApplication.java
@@ -1,17 +1,7 @@
 package com.restaurantes;
 
-import com.restaurantes.model.Dish;
-import com.restaurantes.model.Restaurant;
-import com.restaurantes.model.Review;
-import com.restaurantes.model.enums.DishType;
-import com.restaurantes.model.enums.FoodType;
-import com.restaurantes.repository.DishRepository;
-import com.restaurantes.repository.RestaurantRepository;
-import com.restaurantes.repository.ReviewRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class RestaurantesApplication {
@@ -19,40 +9,40 @@ public class RestaurantesApplication {
     public static void main(String[] args) {
         var context = SpringApplication.run(RestaurantesApplication.class, args);
 
-        // INTRODUCIR DATOS DE PRUEBA PARA LA INTERFAZ DE USUARIO HTML
-        RestaurantRepository restaurantRepository = context.getBean(RestaurantRepository.class);
-        DishRepository dishRepository = context.getBean(DishRepository.class);
-        ReviewRepository reviewRepository =context.getBean(ReviewRepository.class);
-
-        restaurantRepository.save(
-        Restaurant.builder().name("Restaurante 1").averagePrice(null).active(false).foodType(FoodType.SPANISH).build()
-        );
-        restaurantRepository.save(
-        Restaurant.builder().name("Restaurante 2").averagePrice(30.5).foodType(FoodType.MEXICAN).build()
-        );
-        restaurantRepository.save(
-        Restaurant.builder().name("Restaurante 3").averagePrice(40.5).foodType(FoodType.JAPANESE).build()
-        );
-        Restaurant restaurant4 = restaurantRepository.save(
-                Restaurant.builder().name("Restaurante 4").averagePrice(40.5).foodType(null).build()
-        );
-        dishRepository.save(
-                Dish.builder()
-                        .name("Dish 1")
-                        .price(10.5)
-                        .restaurant(restaurant4)
-                        .active(true)
-                        .imageUrl("https://placehold.co/100")
-                        .type(DishType.MAIN_COURSE)
-                        .build()
-        );
-
-        reviewRepository.saveAll(List.of(
-                Review.builder().title("Majestuoso").content("No tengo palabras").rating(5).restaurant(restaurant4).build(),
-                Review.builder().title("Puaj").content("Mejor no te cuento").rating(1).restaurant(restaurant4).build(),
-                Review.builder().title("OK").content("Hola soy Hilario.").rating(3).restaurant(restaurant4).build(),
-                Review.builder().title("Está bueno xd").content("LOL").rating(4).restaurant(restaurant4).build()
-        ));
+//        // INTRODUCIR DATOS DE PRUEBA PARA LA INTERFAZ DE USUARIO HTML
+//        RestaurantRepository restaurantRepository = context.getBean(RestaurantRepository.class);
+//        DishRepository dishRepository = context.getBean(DishRepository.class);
+//        ReviewRepository reviewRepository =context.getBean(ReviewRepository.class);
+//
+//        restaurantRepository.save(
+//        Restaurant.builder().name("Restaurante 1").averagePrice(null).active(false).foodType(FoodType.SPANISH).build()
+//        );
+//        restaurantRepository.save(
+//        Restaurant.builder().name("Restaurante 2").averagePrice(30.5).foodType(FoodType.MEXICAN).build()
+//        );
+//        restaurantRepository.save(
+//        Restaurant.builder().name("Restaurante 3").averagePrice(40.5).foodType(FoodType.JAPANESE).build()
+//        );
+//        Restaurant restaurant4 = restaurantRepository.save(
+//                Restaurant.builder().name("Restaurante 4").averagePrice(40.5).foodType(null).build()
+//        );
+//        dishRepository.save(
+//                Dish.builder()
+//                        .name("Dish 1")
+//                        .price(10.5)
+//                        .restaurant(restaurant4)
+//                        .active(true)
+//                        .imageUrl("https://placehold.co/100")
+//                        .type(DishType.MAIN_COURSE)
+//                        .build()
+//        );
+//
+//        reviewRepository.saveAll(List.of(
+//                Review.builder().title("Majestuoso").content("No tengo palabras").rating(5).restaurant(restaurant4).build(),
+//                Review.builder().title("Puaj").content("Mejor no te cuento").rating(1).restaurant(restaurant4).build(),
+//                Review.builder().title("OK").content("Hola soy Hilario.").rating(3).restaurant(restaurant4).build(),
+//                Review.builder().title("Está bueno xd").content("LOL").rating(4).restaurant(restaurant4).build()
+//        ));
     }
 
 }
--- src/main/java/com/restaurantes/config/DataInitializer.java
+++ src/main/java/com/restaurantes/config/DataInitializer.java
@@ -1,13 +1,44 @@
 package com.restaurantes.config;
 
+import com.restaurantes.model.Dish;
+import com.restaurantes.model.Restaurant;
+import com.restaurantes.model.enums.DishType;
+import com.restaurantes.model.enums.FoodType;
+import com.restaurantes.repository.*;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
+@Profile("!test")
 public class DataInitializer implements CommandLineRunner {
+
+    private DishRepository dishRepo;
+    private RestaurantRepository restaurantRepo;
+    private OrderRepository orderRepo;
+    private OrderLineRepository orderLineRepo;
+    private ReviewRepository reviewRepo;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("HOLA DESDE DATA INITIALIZER");
+
+        if (restaurantRepo.count() > 0) return;
+
+        var res1 = restaurantRepo.save(Restaurant.builder().name("R1").foodType(FoodType.SPANISH).averagePrice(12.50).build());
+        var res2 = restaurantRepo.save(Restaurant.builder().name("R2").foodType(FoodType.JAPANESE).averagePrice(15.50).build());
+        var res3 = restaurantRepo.save(Restaurant.builder().name("R3").foodType(FoodType.MEXICAN).averagePrice(19.50).build());
+
+        var dish1 = dishRepo.save(Dish.builder().type(DishType.MAIN_COURSE).name("Plato1").price(9.99).restaurant(res1).build());
+        var dish2 = dishRepo.save(Dish.builder().type(DishType.STARTER).name("Plato2").price(19.99).restaurant(res1).build());
+        var dish3 = dishRepo.save(Dish.builder().type(DishType.DESSERT).name("Plato3").price(29.99).restaurant(res1).build());
+        var dish4 = dishRepo.save(Dish.builder().type(DishType.STARTER).name("Plato4").price(39.99).restaurant(res2).build());
+        var dish5 = dishRepo.save(Dish.builder().type(DishType.DESSERT).name("Plato5").price(49.99).restaurant(res2).build());
+        var dish6 = dishRepo.save(Dish.builder().type(DishType.MAIN_COURSE).name("Plato6").price(9.99).restaurant(res3).build());
+
+
+
     }
 }
