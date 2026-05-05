8a9b1a0f4ea0f8f919dc9ae810993af67b56987c
alansastre <alan@certidevs.com>
2026-05-05 16:40:28 +0200

    #10 deactivateRestaurant

---
+++ src/main/java/com/restaurantes/controller/api/RestaurantRestController.java
@@ -0,0 +1,9 @@
+package com.restaurantes.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/restaurants")
+public class RestaurantRestController {
+}
