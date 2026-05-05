809b86e6b196477a6274da4de1eb2cf0c35813bb
alansastre <alan@certidevs.com>
2026-05-05 16:12:10 +0200

    #10 deleteReview ok

--- src/test/java/com/restaurantes/controller/ReviewControllerTest.java
+++ src/test/java/com/restaurantes/controller/ReviewControllerTest.java
@@ -2,6 +2,7 @@ package com.restaurantes.controller;
 
 import com.restaurantes.model.Dish;
 import com.restaurantes.model.Restaurant;
+import com.restaurantes.model.Review;
 import com.restaurantes.repository.DishRepository;
 import com.restaurantes.repository.RestaurantRepository;
 import com.restaurantes.repository.ReviewRepository;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -30,13 +32,27 @@ class ReviewControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    Review review1;
+
     @BeforeEach
     void setUp() {
+        review1 = reviewRepo.save(Review.builder().title("OK").rating(5).build());
 
     }
 
     @Test
-    void test1() throws Exception {
+    void deleteReview() throws Exception {
 
+        // /reviews/delete/{id}
+        Long id = review1.getId();
+        assertTrue(reviewRepo.existsById(id));
+
+        mockMvc.perform(get("/reviews/delete/" + id))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/reviews"))
+                .andExpect(flash().attribute("message", "Borrado exitosamente"));
+
+
+        assertFalse(reviewRepo.existsById(id));
     }
 }
