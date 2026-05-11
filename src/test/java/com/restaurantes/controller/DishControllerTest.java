aac8c95cd8c03c90e946416e49f523455164b17c
alansastre <alan@certidevs.com>
2026-05-11 18:01:37 +0200

    #5 DishControllerTest structure TODOs

--- src/test/java/com/restaurantes/controller/DishControllerTest.java
+++ src/test/java/com/restaurantes/controller/DishControllerTest.java
@@ -3,23 +3,41 @@ package com.restaurantes.controller;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// TODO IMPORTS
 
+
+// TODO MOCKMVC
+// TODO TRANSACTIONAL
 @SpringBootTest
 public class DishControllerTest {
-    @Test
-    void list() {
-        Assertions.fail("Pendiente test detail");
-    }
-    @Test
-    void detail() {
-        Assertions.fail("Pendiente test detail");
-    }
+    // TODO MOCKMVC
+    // TODO REPOS
+//    @Test
+//    void list() {
+//        Assertions.fail("Pendiente test detail");
+//    }
+//    @Test
+//    void detail() {
+//        Assertions.fail("Pendiente test detail");
+//    }
     @Test
     void create() {
+        // count
+
+        // mockmvc perform post /dishes
+
+        // count + 1
+        // findById y comprobar datos
         Assertions.fail("Pendiente test crear nuevo Plato");
     }
     @Test
     void edit() {
+        // save
+
+        // mockmvc perform post cambiando nombre precio restaurante apuntando al mismo id /dishes
+
+        // findById y comprobar datos
+        // count no debe aumentar
         Assertions.fail("Pendiente test editar Plato existente");
     }
 }
