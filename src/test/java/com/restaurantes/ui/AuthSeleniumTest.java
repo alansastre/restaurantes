87e1b4e86b1311b47e41cffe63c4336ec9d95ed5
alansastre <alan@certidevs.com>
2026-06-05 17:29:04 +0200

    Selenium logout

--- src/test/java/com/restaurantes/ui/AuthSeleniumTest.java
+++ src/test/java/com/restaurantes/ui/AuthSeleniumTest.java
@@ -2,8 +2,10 @@ package com.restaurantes.ui;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AuthSeleniumTest extends BaseSeleniumTest {
 
@@ -27,5 +29,27 @@ public class AuthSeleniumTest extends BaseSeleniumTest {
         driver.findElement(By.cssSelector("button[type='submit']")).click();
         wait.until(driver -> driver.getCurrentUrl().equals(baseUrl + "login"));
         assertEquals(baseUrl + "login", driver.getCurrentUrl());
+    }
+
+    @Test
+    void logout() {
+        driver.get(baseUrl + "restaurants");
+
+        // Primero sin autenticarse
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("loginLink"))));
+        assertEquals(1, driver.findElements(By.id("loginLink")).size()); // SI LOGIN
+        assertEquals(1, driver.findElements(By.id("registerLink")).size()); // SI REGISTRO
+        assertTrue(driver.findElements(By.id("logoutButton")).isEmpty()); // NO LOGOUT
+
+        loginUser();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("logoutButton"))));
+        assertTrue(driver.findElements(By.id("loginLink")).isEmpty()); // NO LOGIN
+        assertTrue(driver.findElements(By.id("registerLink")).isEmpty()); // NO REGISTRO
+        assertEquals(user.getUsername(), driver.findElement(By.id("username")).getText()); // SI LOGOUT
+
+        driver.findElement(By.id("logoutButton")).click();
+        wait.until(ExpectedConditions.urlContains("/login?logout"));
+
     }
 }
