0f39bbd809473c3532219a40eed5a1d523f27b37
alansastre <alan@certidevs.com>
2026-06-08 16:41:27 +0200

    API REST restaurants get mapping list

--- src/main/java/com/restaurantes/controller/api/RestaurantRestController.java
+++ src/main/java/com/restaurantes/controller/api/RestaurantRestController.java
@@ -1,9 +1,23 @@
 package com.restaurantes.controller.api;
 
+import com.restaurantes.model.Restaurant;
+import com.restaurantes.repository.RestaurantRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+// http://localhost:8080/swagger-ui/index.html
 @RestController
-@RequestMapping("/api/restaurants")
+@RequestMapping("/api/v1/restaurants")
+@AllArgsConstructor
 public class RestaurantRestController {
+
+    private final RestaurantRepository restaurantRepository;
+
+    @GetMapping
+    public List<Restaurant> findAll() {
+        return restaurantRepository.findAll();
+    }
 }
